9c461b5ec27d32f9bdba743e01a49ab9c04bf340
hourglasshoro <hourglasshoro.628@gmail.com>
2023-04-06 08:28:14 +0900

    fix: remove unused import

--- test/LearnToEarn.ts
+++ test/LearnToEarn.ts
@@ -1,4 +1,4 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { Contract } from 'ethers';
