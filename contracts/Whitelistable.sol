692fb90b6e46feef2b25e662e55dc5032e9d5fb5
hourglasshoro <hourglasshoro.628@gmail.com>
2023-04-06 08:07:17 +0900

    fix: format

--- contracts/Whitelistable.sol
+++ contracts/Whitelistable.sol
@@ -2,41 +2,41 @@
 
 pragma solidity ^0.8.9;
 
-import "./interfaces/IRegistry.sol";
+import './interfaces/IRegistry.sol';
 
 abstract contract Whitelistable {
-    IRegistry public registry;
+  IRegistry public registry;
 
-    event RegistryUpdated(IRegistry indexed registry);
+  event RegistryUpdated(IRegistry indexed registry);
 
-    constructor(IRegistry _registry) {
-        _setRegistry(_registry);
-    }
+  constructor(IRegistry _registry) {
+    _setRegistry(_registry);
+  }
 
-    modifier onlyWhitelisted() {
-        _checkWhitelisted(msg.sender);
-        _;
-    }
+  modifier onlyWhitelisted() {
+    _checkWhitelisted(msg.sender);
+    _;
+  }
 
-    modifier ifWhitelisted(address account) {
-        _checkWhitelisted(account);
-        _;
-    }
+  modifier ifWhitelisted(address account) {
+    _checkWhitelisted(account);
+    _;
+  }
 
-    function updateRegistry(IRegistry _registry) public virtual {
-        _setRegistry(_registry);
-    }
+  function updateRegistry(IRegistry _registry) public virtual {
+    _setRegistry(_registry);
+  }
 
-    function _setRegistry(IRegistry _registry) internal {
-        registry = _registry;
-        emit RegistryUpdated(_registry);
-    }
+  function _setRegistry(IRegistry _registry) internal {
+    registry = _registry;
+    emit RegistryUpdated(_registry);
+  }
 
-    function _checkWhitelisted(address account) internal view {
-        if (!registry.isWhitelisted(account)) revert MemberNotWhitelisted(msg.sender);
-    }
+  function _checkWhitelisted(address account) internal view {
+    if (!registry.isWhitelisted(account)) revert MemberNotWhitelisted(msg.sender);
+  }
 
-    function _checkWhitelisted(address[] memory accounts) internal view returns (bool) {
-        return registry.areWhitelisted(accounts);
-    }
+  function _checkWhitelisted(address[] memory accounts) internal view returns (bool) {
+    return registry.areWhitelisted(accounts);
+  }
 }
--- contracts/interfaces/IRegistry.sol
+++ contracts/interfaces/IRegistry.sol
@@ -9,11 +9,17 @@ error NotWhitelisted();
 /// @notice This is the interface for a registry contract
 /// @author thev
 interface IRegistry {
-    function isWhitelisted(address member) external view returns (bool);
-    function areWhitelisted(address[] calldata member) external view returns (bool);
-    function bulkAddToWhitelist(address[] calldata members) external;
-    function addToWhitelist(address member) external;
-    function bulkRemoveFromWhitelist(address[] calldata members) external;
-    function removeFromWhitelist(address member) external;
-    function grantRoleAdmin(address account) external;
+  function isWhitelisted(address member) external view returns (bool);
+
+  function areWhitelisted(address[] calldata member) external view returns (bool);
+
+  function bulkAddToWhitelist(address[] calldata members) external;
+
+  function addToWhitelist(address member) external;
+
+  function bulkRemoveFromWhitelist(address[] calldata members) external;
+
+  function removeFromWhitelist(address member) external;
+
+  function grantRoleAdmin(address account) external;
 }
