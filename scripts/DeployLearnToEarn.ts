e822333eb4d941f03aeb0a11adec22ec735c6ef5
hourglasshoro <hourglasshoro.628@gmail.com>
2023-04-06 07:55:14 +0900

    fix: deploy script

--- scripts/DeployLearnToEarn.ts
+++ scripts/DeployLearnToEarn.ts
@@ -5,10 +5,18 @@ dotenv.config()
 async function main() {
   const contract = await ethers.getContractFactory('LearnToEarn')
   const token = await contract.deploy(
+    process.env.REGISTRY_ADDRESS as string, // Please note the address of the registry when deploying cJPY and set it here
     process.env.CIT_COIN_ADDRESS as string,
     process.env.GNOSIS_OWNER as string,
   )
   await token.deployed()
+
+  const registryFactory = await ethers.getContractFactory('Registry')
+  const registry = await registryFactory.attach(
+      process.env.REGISTRY_ADDRESS ?? ''
+  )
+  // Necessary to operate a registry contract from learn to earn contract
+  await registry.grantRoleAdmin(token.address)
 
   console.log('Token: ', token.address)
   await token.transferOwnership(process.env.GNOSIS_OWNER as string)
