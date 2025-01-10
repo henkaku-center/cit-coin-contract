40d33df4a836c30a0c9f44fff51b47839e595fde
Hokuto Morita <han2210mh@gmail.com>
2025-01-10 16:25:04 +0900

    [clean]tmp

--- scripts/deployFaucet.ts
+++ scripts/deployFaucet.ts
@@ -18,4 +18,3 @@ main().catch((error) => {
   console.error(error)
   process.exitCode = 1
 })
-//
--- scripts/deployLearnToEarn.ts
+++ scripts/deployLearnToEarn.ts
@@ -26,4 +26,3 @@ main().catch((error) => {
   console.error(error)
   process.exitCode = 1
 })
-//
